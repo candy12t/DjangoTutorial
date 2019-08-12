213493e4f25d691d9cceb2c826745fa205e90f8b
candy12t <candy12t@gmail.com>
2019-08-12 13:13:51 +0900

    Correction Choice

--- mysite/polls/models.py
+++ mysite/polls/models.py
@@ -5,6 +5,6 @@ class Question(models.Model):
   pub_date = models.DateTimeField('date published')
 
 class Choice(models.Model):
-  question = models.ForeignKey(Question, on_date=models.CASCADE)
+  question = models.ForeignKey(Question, on_delete=models.CASCADE)
   choice_text = models.CharField(max_length=200)
   votes = models.IntegerField(default=0)
