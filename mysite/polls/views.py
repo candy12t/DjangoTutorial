9cda1d171f97fca7108de5f56401e312d1e3d870
candy12t <candy12t@gmail.com>
2019-08-16 17:21:43 +0900

    Correct polls/views

--- mysite/polls/views.py
+++ mysite/polls/views.py
@@ -1,6 +1,6 @@
 from django.http import HttpResponse, HttpResponseRedirect
 from .models import Question, Choice
-from django.shortcuts import render, get_list_or_404
+from django.shortcuts import render, get_object_or_404
 from django.urls import reverse
 
 def index(request):
