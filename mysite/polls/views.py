210a12cb292766b893c594257ec096cc7e47d16f
candy12t <candy12t@gmail.com>
2019-08-17 23:59:28 +0900

    修正

--- mysite/polls/views.py
+++ mysite/polls/views.py
@@ -4,7 +4,7 @@ from django.shortcuts import render, get_object_or_404
 from django.urls import reverse
 from django.views import generic
 
-class IndexView(generic/ListView):
+class IndexView(generic.ListView):
   template_name = 'polls/index.html'
   context_object_name = 'latest_question_list'
 
@@ -15,7 +15,7 @@ class DetailView(generic.DetailView):
   model = Question
   template_name = 'polls/detail.html'
 
-class ResultsView(generic):
+class ResultsView(generic.DetailView):
   model = Question
   template_name = 'polls/results.html'
 
